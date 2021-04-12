5812ff5ac933f04e1a0db1549f9e3a5ea3177a62
hassan797 <69317930+hassan797@users.noreply.github.com>
2021-04-12 14:00:06 +0300

    views register/login fcts

--- appointments/views.py
+++ appointments/views.py
@@ -1,5 +1,8 @@
+import django.http
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.template.context_processors import csrf
 from .forms import *
@@ -8,6 +11,39 @@ from datetime import datetime
 from django.utils import timezone
 # Create your views here.
 
+
+def signup(request):
+
+    form = UserForm()
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("") # take user to home page
+    return render(request, "appointment/register.html", {'form': form})
+
+
+def loginuser(request):
+
+    print(request.user.username)
+    if request.method == "POST":
+        username = request.POST.get("username")
+        passwrd = request.POST.get("password")
+        member = User.objects.get(username=request.POST['username'])
+        if member.password== passwrd :
+            request.session['user_id'] = member.id
+            login(request, member)
+            return HttpResponseRedirect('')
+        else:
+            messages.info(request, "username or password is incorect")
+    return render(request, "")
+
+
+def logoutuser(request):
+    for i in list(request.session.keys()):
+        del request.session[i]
+    logout(request)
+    return redirect("loginuser")
 
 
 def ViewAppointments( request):
