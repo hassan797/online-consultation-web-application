d781a22fd1a6df1660dfa4d2965db88fcdbc1852
hassan797 <69317930+hassan797@users.noreply.github.com>
2021-04-14 22:53:34 +0300

    urls

--- home/urls.py
+++ home/urls.py
@@ -4,11 +4,9 @@ from . import views
 
 #MUST HAVE SAME NAME AS HTML TEMPLATES
 urlpatterns = [
-    url(r'^$',views.index),
-    url(r'^login', views.login),
-    url(r'^register', views.register, name = "home-register"),
-#     url(r'^logout',views.logout),
-    url(r'^HomePage', views.HomePage, name = "home"),
-   
+    url(r'^$', views.HomePage),
+    #     url(r'^login',views.login),
+    url(r'^register', views.register),
+    #     url(r'^logout',views.logout),
 
 ]
